91eb0acb5142db45e4f889f580e010e3b5757adf
abalbissi <116764176+abalbissi@users.noreply.github.com>
2022-12-13 17:43:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const bar = document.getElementById('bar')
-// const nav = document.getElementById('menu')
-// var close = document.getElementById('close')
+const nav = document.getElementById('menu')
+var close = document.getElementById('close')
 
-// if (bar) {
-//     bar.addEventListener('click', () => {
-//         nav.classList.add("active")
-//     })
-// }
-// if (nav) {
-//     close.addEventListener('click', () =>{
-//         nav.classList.remove("active")
-//     })
-// }
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add("active")
+    })
+}
+if (nav) {
+    close.addEventListener('click', () =>{
+        nav.classList.remove("active")
+    })
+}
